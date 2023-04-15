92e4a37ddedf34108c541fb4cbb559c6c1efe375
Sam786968 <samzajc@gmail.com>
2023-04-15 16:34:06 -0500

    Co-authored-by: mat212121 <mat212121@users.noreply.github.com> Co-authored-by: asalazar23 <asalazar23@users.noreply.github.com> Co-authored-by: nasolis <nasolis@users.noreply.github.com> Co-authored-by: jrdelgado3 <jrdelgado3@users.noreply.github.com> Co-authored-by: nasolis665 <nasolis665@users.noreply.github.com>

--- app.py
+++ app.py
@@ -1,20 +1,15 @@
 from flask import Flask, render_template
-from main import *
-app =Flask(__name__)
+import json
+import random
+import time
+import threading
 
+app= Flask(__name__)
 @app.route('/')
 def index():
-    quote = print_random_quote()
-    return render_template('index.html', quote=quote)
+    print("I got clicked!")
 
-if __name__=='__main__':
-    app.run()
-    
-    
-def print_random_quote():
-    with open('quotes.json') as f:
-        data=json.load(f)
-    quote=random.choice(data['quotes'])['text']
-    print(quote)
+    return "Click."
 
-    threading.Timer(10, print_random_quote).start()
+if __name__ == '__main__':
+    app.run(debug=True)
--- donors.py
+++ donors.py
@@ -1,4 +1,3 @@
-from main import *
 import json
 import csv
 
@@ -12,11 +11,11 @@ with open('donors_information.csv', 'w', encoding = 'utf-8', newline='') as file
     writer = csv.writer(file)
     writer.writerow(csv_headers)
 
-list1 = ['1000','584459865','Joe Smith'].lower()
-list2 = ['1000','584459654','Jane Doe'].lower()
-list3 = ['1000','584659765','Greg Gardner'].lower()
-list4 = ['1000','585459645','Nathan Solis'].lower()
-list5 = ['1000','583459615','Mateo Chavez'].lower()
+list1 = ['1000','584459865','joe smith']
+list2 = ['1500','584459654','jane doe']
+list3 = ['2000','584659765','greg gardner']
+list4 = ['3000','585459645','nathan solis']
+list5 = ['5000','583459615','mateo chavez']
 lists = [list1, list2, list3, list4, list5]
 
 with open('donors_information.csv', 'a', encoding = 'utf-8', newline='') as file:
@@ -33,20 +32,22 @@ from email import encoders
 
 # This gives me the information I am looking for and looking through. 
 question = input('What is your name? ').lower()
-with open(filename, 'r', newline='', encoding='utf-8') as file:
+with open('donors_information.csv', 'r', newline='', encoding='utf-8') as file:
     reader = csv.reader(file)
     
 # This finds if the person on the page is a donor, then retrieves the other data for the email if they are.
-for row in reader:
-    if question in reader:
-        name = question
-        # amount = Amount Donated
-        # ein = EIN Number
-    else:
-        print("You are not a donator.")
+    for row in reader:
+        if question in row:
+            # print(question)
+            amount = row[0] 
+            # print(f'amount is {amount}')
+            einum = row[1] 
+            # print(f'amount is {einum}')
+        else:
+            None
 
 email = ("samzajc@gmail.com")
-message = (f"Here is your reciept. {name}, with the EIN {ein}, has donated {amount}")
+message = (f"Here is your reciept. {question}, with the EIN {einum}, has donated {amount}")
 msg = MIMEMultipart()
 msg['From'] = "nasolis600@gmail.com"
 msg['To'] = email
@@ -59,6 +60,19 @@ part.set_payload((attachment).read())
 encoders.encode_base64(part)
 part.add_header('Content-Disposition', "attachment; filename= %s" % filename)
 msg.attach(part)
+server = smtplib.SMTP('smtp.gmail.com', 587)
+server.starttls()
+server.login("nasolis600@gmail.com", "feieafxmkmijbfzf")
+text = msg.as_string()
+server.sendmail(msg['From'], msg['To'], text)
+server.quit()
+print("Your donation has been received, thank you for your support!")
+
+email = ("samzajc@gmail.com")
+message = (f"Here is your reciept. {question}, with the EIN {einum}, has donated {amount}")
+msg['From'] = "nasolis600@gmail.com"
+msg['To'] = email
+msg['Subject'] = "Reciept for donation."
 server = smtplib.SMTP('smtp.gmail.com', 587)
 server.starttls()
 server.login("nasolis600@gmail.com", "feieafxmkmijbfzf")
