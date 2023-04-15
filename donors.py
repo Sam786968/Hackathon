d069f6a6cfb471b5a9bcdea8f491c9eb240ac1a2
Sam786968 <samzajc@gmail.com>
2023-04-15 18:49:56 -0500

    backend is 100 percent frontend is 33 percent

--- donors.py
+++ donors.py
@@ -51,66 +51,53 @@ with open('donors_information.csv', 'a', encoding = 'utf-8', newline='') as file
 # This gives me the information I am looking for and looking through. 
 question = input('What is your name? ').lower()
 
+def attached_emails(question,amount,einum):
+    # Attachment for organization.
+    email = ("samzajc@gmail.com")
+    message = (f"Here is your reciept. {question}, with the EIN {einum}, has donated {amount}")
+    msg=MIMEMultipart()
+    msg['From'] = "nasolis600@gmail.com"
+    msg['To'] = email
+    msg['Subject'] = "Reciept for donation."
+    msg.attach(MIMEText(message, 'plain'))
+    filename="donors_information.csv"
+    attachment=open(filename, 'rb')
+    part=MIMEBase('application', 'octet-stream')
+    part.set_payload((attachment).read())
+    encoders.encode_base64(part)
+    part.add_header('Content-Disposition', "attachment; filename= %s" % filename)
+    msg.attach(part)
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server.starttls()
+    server.login("nasolis600@gmail.com", "feieafxmkmijbfzf")
+    text = msg.as_string()
+    server.sendmail(msg['From'], msg['To'], text)
+    server.quit()
+    # Email for donor.
+   
+def email(question,amount,einum):
+    email = ("samzajc@gmail.com")
+    message = (f"Here is your reciept. {question}, with the EIN {einum}, has donated {amount}. Thank you so much for your donation, have a great day.")
+    msg=MIMEMultipart()
+    msg['From'] = "nasolis600@gmail.com"
+    msg['To'] = email
+    msg['Subject'] = "Reciept for donation."
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server.starttls()
+    server.login("nasolis600@gmail.com", "feieafxmkmijbfzf")
+    text = msg.as_string()
+    server.sendmail(msg['From'], msg['To'], text)
+    server.quit()
+
 with open('donors_information.csv', 'r', newline='', encoding='utf-8') as file:
     reader = csv.reader(file)
     for row in reader:
-        print(row)
         if question in row:
             amount = row[0] 
             einum = row[1] 
-            
-            # send email
-            sender_email = 'nasolis600@gmail.com'
-            sender_password = 'feieafxmkmijbfzf'
-            recipient_email = 'samzajc@gmail.com' # email address from the CSV file
-            smtp_server = 'smtp.gmail.com'
-            smtp_port = 587
-            message = f"Here is your receipt. {question}, with the EIN {einum}, has donated {amount}."
-            msg = MIMEMultipart()
-            msg['From'] = "nasolis600@gmail.com"
-            msg['To'] = recipient_email
-            msg['Subject'] = "Receipt for donation."
-            msg.attach(MIMEText(message, 'plain'))
-            
-            filename = "donors_information.csv"
-            attachment = open(filename, "rb")
-            part = MIMEBase('application', 'octet-stream')
-            part.set_payload((attachment).read())
-            encoders.encode_base64(part)
-            part.add_header('Content-Disposition', "attachment; filename= %s" % filename)
-            msg.attach(part)
-            
-            email = ("samzajc@gmail.com")
-            message = (f"Here is your reciept. {question}, with the EIN {einum}, has donated {amount}. Thank you so much for your donation, have a great day.")
-            msg['From'] = "nasolis600@gmail.com"
-            msg['To'] = email
-            msg['Subject'] = "Reciept for donation."
-            server = smtplib.SMTP('smtp.gmail.com', 587)
-            server.starttls()
-            server.login("nasolis600@gmail.com", "feieafxmkmijbfzf")
-            text = msg.as_string()
-            server.sendmail(msg['From'], msg['To'], text)
-            server.quit()
-        
-            # exit the loop after finding the matching row
+            attached_emails(question,amount,einum)
+            email(question,amount,einum)
             print("Your donation has been received, thank you for your support!")
-            break
         else:
             None
-            break
-            
-
-
-# To Donor
-# email = ("samzajc@gmail.com")
-# message = (f"Here is your reciept. {question}, with the EIN {einum}, has donated {amount}. Thank you so much for your donation, have a great day.")
-# msg['From'] = "nasolis600@gmail.com"
-# msg['To'] = email
-# msg['Subject'] = "Reciept for donation."
-# server = smtplib.SMTP('smtp.gmail.com', 587)
-# server.starttls()
-# server.login("nasolis600@gmail.com", "feieafxmkmijbfzf")
-# text = msg.as_string()
-# server.sendmail(msg['From'], msg['To'], text)
-# server.quit()
-
+        
